3198252d188593e56a42fc8a329a9656048b5f68
archetypiarz <archetypiarz@gmail.com>
2025-12-08 13:15:28 +0100

    remove subproject layout and add path to configure in build

--- src/some_binary/conanfile.py
+++ src/some_binary/conanfile.py
@@ -14,8 +14,6 @@ class some_binary(ConanFile):
         self.requires("fmt/12.1.0")
 
     def layout(self):
-        self.folders.root = "../.."
-        self.folders.subproject = "src/some_binary"
         cmake_layout(self)
 
     def export(self):
@@ -28,6 +26,6 @@ class some_binary(ConanFile):
         
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure("src/some_binary")
         cmake.build()
         self.run(os.path.join(self.cpp.build.bindirs[0], "some_binary"))
